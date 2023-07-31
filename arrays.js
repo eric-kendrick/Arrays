f0442025fd027e95eb2367289ec91461561c1791
Eric Kendrick <eric.c.kendrick@gmail.com>
2023-07-31 17:57:18 -0500

    Add string, number and boolean arrays to arrays file

---
+++ arrays.js
@@ -0,0 +1,7 @@
+
+// Declare a variable that stores an array of at least four strings
+let pals = ["Ben", "Brent", "Elise", "Eric", "Tammy"];
+// Declare a variable that stores an array of at least four numbers
+let ages = [39, 36, 41, 35, 37];
+//Declare a variable that stores an array of at least four booleans
+let boolArr = [false, true, true, false];
