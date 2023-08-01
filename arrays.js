f8a580c39df57d06873dfe0c50a3847dd00d75aa
Eric Kendrick <eric.c.kendrick@gmail.com>
2023-07-31 21:35:48 -0500

    Add comments for index position explanation

--- arrays.js
+++ arrays.js
@@ -20,3 +20,13 @@ with an arrow function. Function will set parameters to filter out names under
 five characters in length
 // */
 console.log(pals.filter(pals => pals.length > 4));
+
+//The Index Position
+
+/* The indices of an array denote a particular element's position within the array.
+Javascript arrays are zero-indexed, which means the first element of the array is 
+positioned at index 0, the second at index 1, and so on. These indices are important 
+when using certain methods, such as the previously called splice method. An element can
+be accessed by its respective index using bracket notation. For example, in the the
+"pals" array, I can console log the element "Ben" using console.log(pals[0]). */
+console.log(pals[0]); //returns Ben
